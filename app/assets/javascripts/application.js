102337f729a4487a446646fad4a7ea43b33a4920
jay <lnewhenio@gmail.com>
2017-05-24 10:07:56 +0000

    added maps to the page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,18 +14,3 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-
-var handler = Gmaps.build('Google');
-handler.buildMap({ internal: {id: 'geolocation'} }, function(){
-  // be aware chrome >= 50 requires https for geolocation to work
-  if(navigator.geolocation)
-    navigator.geolocation.getCurrentPosition(displayOnMap);
-});
-
-function displayOnMap(position){
-  var marker = handler.addMarker({
-    lat: position.coords.latitude,
-    lng: position.coords.longitude
-  });
-  handler.map.centerOn(marker);
-};
--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -2,17 +2,9 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
 
-var handler = Gmaps.build('Google');
-handler.buildMap({ internal: {id: 'geolocation'} }, function(){
-  // be aware chrome >= 50 requires https for geolocation to work
-  if(navigator.geolocation)
-    navigator.geolocation.getCurrentPosition(displayOnMap);
-});
 
-function displayOnMap(position){
-  var marker = handler.addMarker({
-    lat: position.coords.latitude,
-    lng: position.coords.longitude
-  });
-  handler.map.centerOn(marker);
-};
+
+
+
+
+
