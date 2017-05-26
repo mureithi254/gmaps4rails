030fd60ee2eb2745d249cc94e76f9abe0429ed31
jay <lnewhenio@gmail.com>
2017-05-26 12:55:15 +0000

    modified the number of markers on the map

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -3,6 +3,22 @@
 # You can use CoffeeScript in this file: http://coffeescript.org/
 
 
+var handler = Gmaps.build('Google');
+handler.buildMap({ internal: {id: 'geolocation'} }, function(){
+  // be aware chrome >= 50 requires https for geolocation to work
+  if(navigator.geolocation)
+    navigator.geolocation.getCurrentPosition(displayOnMap);
+});
+
+function displayOnMap(position){
+  var marker = handler.addMarker({
+    lat: position.coords.latitude,
+    lng: position.coords.longitude
+  });
+  handler.map.centerOn(marker);
+};
+
+
 
 
 
