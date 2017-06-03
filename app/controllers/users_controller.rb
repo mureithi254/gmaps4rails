72554b0bf9c747bcbc913199b35893ce362a1586
jay <lnewhenio@gmail.com>
2017-06-03 13:57:58 +0000

    added and configured gmaps4rails to take in data from the database in json format

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,16 @@ class UsersController < ApplicationController
   # GET /users.json
   def index
     @users = User.all
+    @hash = Gmaps4rails.build_markers(@users) do |user, marker|
+      marker.lat user.latitude
+      marker.lng user.longitude
+       marker.infowindow user.description
+       marker.picture({
+       "url" => "http://people.mozilla.com/~faaborg/files/shiretoko/firefoxIcon/firefox-32.png",
+       "width" =>  32,
+       "height" => 32})
+       marker.json({ title: user.title })
+    end
   end
 
   # GET /users/1
