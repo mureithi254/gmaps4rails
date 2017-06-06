2f5fe0c60b19723650a78eb7d2b3e7494888a22c
jay <lnewhenio@gmail.com>
2017-06-06 09:44:09 +0000

    customised form and added jquery for dropdown

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users
   get 'users/index'
   root 'maps#view'
+  get 'maps/view'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
